e415b402dba52d225ca140a6f2e47a5b84ce49a7
sergiua <contact@sergiuavram.com>
2023-12-09 16:23:58 +0200

    first commit

---
+++ Nume_Pren_Lab7/AboutPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace Nume_Pren_Lab7;
+
+public partial class AboutPage : ContentPage
+{
+	public AboutPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Nume_Pren_Lab7/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Nume_Pren_Lab7
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
